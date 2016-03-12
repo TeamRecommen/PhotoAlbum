8d81670aa7a42fd9da8017fdce4af90e9b828d89
AsenTahchiyski <malkstor@gmail.com>
2016-03-12 17:55:32 +0200

    Aded routing using sammy.js.

--- ProjectFiles/js/app.js
+++ ProjectFiles/js/app.js
@@ -1,9 +1,34 @@
 var app = app || {};
 
 (function (scope) {
+    //console.log(scope);
 
-    console.log(scope);
+    // sammy routing
+    app.router = Sammy(function() {
+        var selector = $('.main-section');
 
+        this.get('#/', function() {
+            $.get('templates/homeTemplate.html', function(content) {
+                $(selector).html(content);
+            });
+        });
 
+        this.get('#/albums', function() {
+            $.get('templates/albumsTemplate.html', function(content) {
+                $(selector).html(content);
+            });
+        });
+
+        this.get('#/about', function() {
+            $.get('templates/aboutTemplate.html', function(content) {
+                $(selector).html(content);
+            });
+        });
+
+        this.get('#/login', function() {
+        });
+    });
+
+    app.router.run('#/');
 })(app);
 
