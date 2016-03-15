7daea5c2ceca612fbff640c8e415948e15347e1a
AsenTahchiyski <malkstor@gmail.com>
2016-03-15 11:14:30 +0200

    Added pictures view and model.

---
+++ ProjectFiles/js/models/picturesModel.js
@@ -0,0 +1,22 @@
+var app = app || {};
+
+app.picturesModel = (function () {
+    function PicturesModel(requester) {
+        this.requester = requester;
+        this.serviceUrl = this.requester.baseUrl + 'appdata/' + this.requester.appId + '/Pictures';
+    }
+
+    PicturesModel.prototype.getAllPictures = function () {
+        return this.requester.get(this.serviceUrl, true)
+    };
+
+    PicturesModel.prototype.addNewPicture = function (picture) {
+        return this.requester.post(this.serviceUrl, picture, true)
+    };
+
+    return {
+        load: function (requester) {
+            return new PicturesModel(requester)
+        }
+    }
+})();
--- ProjectFiles/js/views/picturesViews.js
+++ ProjectFiles/js/views/picturesViews.js
@@ -1,3 +1,42 @@
-/**
- * Created by Ivan on 13.3.2016 г..
- */
+var app = app || {};
+
+app.pictureViews = (function () {
+    function PictureViews() {
+    }
+
+    PictureViews.prototype.showPictures = function (data) {
+        $.get('templates/picturesTemplate.html', function (templ) {
+            var json = {
+                pictures: data
+            };
+            var rendered = Mustache.render(templ, json);
+            $('.main-section').html(rendered);
+            $('#add-picture').on('click', function () {
+                var createPictureDiv = $('<div>')
+                    .addClass('add-picture-form')
+                    .append($('<input>').attr('placeholder', 'Picture Name').attr('id', 'picture-name'))
+                    .append($('<button>').text('Add').on('click', function () {
+                        var name = $('#picture-name').val();
+
+                        $.sammy(function () {
+                            this.trigger('add-picture', {name: name})
+                        })
+                    }));
+                $(this).parent().empty().append(createPictureDiv);
+            });
+        });
+    };
+
+    PictureViews.prototype.showTopPictures = function (data) {
+        $.get('templates/homeTemplate', function (templ) {
+            var rendered = Mustache.render(templ, data);
+            $('.main-section').html(rendered);
+        })
+    };
+
+    return {
+        load: function () {
+            return new PictureViews();
+        }
+    }
+})();
