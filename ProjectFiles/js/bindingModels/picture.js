d4b1fcfb1b4eb4358bedca31c04c19d2f5d7b17b
IvanMladenov <ismladenov@gmail.com>
2016-03-15 11:51:10 +0200

    add code to pictureInputModel

--- ProjectFiles/js/bindingModels/picture.js
+++ ProjectFiles/js/bindingModels/picture.js
@@ -1,9 +1,22 @@
 var app = app || {};
 
 (function (scope) {
-    var PictureInputModel = function(name) {
+    var PictureInputModel = function(name, albumId, comment, data) {
         this._name = name;
-        this._content = '';
+        this._albumId = albumId;
+        this.rating = 0;
+        this.comment = comment;
+        this._data = data;
+    };
+
+    PictureInputModel.prototype.getPictureInputModel = function getPictureInputModel(){
+        return {
+            name: this._name,
+            base64data: this._data,
+            comment: this.comment,
+            rating: this.rating,
+            albumId: this._albumId
+        }
     };
 
     scope.pictureInputModel = function(name) {
