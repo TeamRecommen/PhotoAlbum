1b8025a161f11959706398a610c658e02de19774
monty-georgiev <monty.georgiev@gmail.com>
2016-03-16 08:08:14 +0200

    Overlay
    Added image holder to overlay;

--- ProjectFiles/js/helpers/galleryPopup.js
+++ ProjectFiles/js/helpers/galleryPopup.js
@@ -4,13 +4,17 @@ var app = app || {};
 app.galleryPopup = (function () {
 
     $('.single-picture').on('click', function () {
-        var overlay = $('<div>').addClass('gallery-overlay');
-        var closeOverlay = $('<span>').addClass('close-overlay').on('click', function () {
-            overlay.hide();
-        });
-        var closeImage = $('<img>').attr('src', 'img/close_button.png');
+        var overlay = $('<div>').addClass('gallery-overlay'),
+            closeOverlay = $('<span>')
+                .addClass('close-overlay')
+                .on('click', function () {
+                    overlay.hide();
+                }),
+            closeImage = $('<img>').attr('src', 'img/close_button.png'),
+            imageHolder = $('<div>').addClass('single-image_holder');
+
         closeOverlay.append(closeImage);
-        overlay.append(closeOverlay);
+        overlay.append(closeOverlay, imageHolder);
         $('.main-section').append(overlay);
     })
 
