2fae33168fc389cca1e8f817a05b65550c5c271b
AsenTahchiyski <malkstor@gmail.com>
2016-03-16 11:10:35 +0200

    Added picture rating in album view and like button in popup.

--- ProjectFiles/js/helpers/galleryPopup.js
+++ ProjectFiles/js/helpers/galleryPopup.js
@@ -1,9 +1,9 @@
 var app = app || {};
 
 
-app.galleryPopup = (function (data) {
-    var pictures = $('.single-picture');
-    pictures.on('click', function () {
+app.galleryPopup = (function () {
+
+    $('.single-picture').on('click', function () {
         var overlay = $('<div>').addClass('gallery-overlay'),
             closeOverlay = $('<span>')
                 .addClass('close-overlay')
@@ -16,11 +16,14 @@ app.galleryPopup = (function (data) {
         var imagePreview = $('<img>').attr('src', imageSrc).addClass('image-preview');
 
         var commentSection = $('<div>').addClass('comment-section').text('Comment section here');
+        var ratingSection = $('<div>').addClass('picture-rating-popup').text('Like');
+        var likeButton = $('<button>').addClass('picture-like-button').html('<img src="img/like.png" alt="Like">');
 
         closeOverlay.append(closeImage);
+        ratingSection.append(likeButton);
+        commentSection.append(ratingSection);
         imageHolder.append(imagePreview, closeOverlay, commentSection);
         overlay.append(imageHolder);
         $('.main-section').append(overlay);
     })
-
 });
